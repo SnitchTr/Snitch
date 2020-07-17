3d780c8b9868a58cf1ff08ac48510bbf7bd367e9
ArnauOrtega <62354394+ArnauOrtega@users.noreply.github.com>
2020-07-17 12:06:30 +0200

    didn't work so i fixed it
    Also added a message when no humans are found

--- ir_camera/TFLite_image_detection.py
+++ ir_camera/TFLite_image_detection.py
@@ -6,13 +6,14 @@ import numpy as np
 import sys
 import glob
 import importlib.util
+import shutil
 from picamera import PiCamera
-def humancheck():
+def humancheck(self):
     human = False
     #define a name for the snapshot by giving a number
-    img = "/home/pi/Desktop/snapshots/"+ str(pic_name) + ".jpg"
-    camera.capture(img)
-    pic_name= pic_name +1
+    img = "/home/pi/Desktop/snapshots/"+ str(self.pic_name) + ".jpg"
+    self.camera.capture(img)
+    self.pic_name= self.pic_name +1
     # Define and parse input argumets
     MODEL_NAME = 'Sample_TFLite_model'
     GRAPH_NAME = 'detect.tflite'
@@ -30,40 +31,40 @@ def humancheck():
         from tensorflow.lite.python.interpreter import Interpreter
 
         # Get path to current working directory
-        CWD_PATH = os.getcwd()
+    CWD_PATH = os.getcwd()
 
         # Path to .tflite file, which contains the model that is used for object detection
-        PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
+    PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
 
         # Path to label map file
-        PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
+    PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
 
         # Load the label map
-        with open(PATH_TO_LABELS, 'r') as f:
-            labels = [line.strip() for line in f.readlines()]
+    with open(PATH_TO_LABELS, 'r') as f:
+        labels = [line.strip() for line in f.readlines()]
 
             # Have to do a weird fix for label map if using the COCO "starter model" from
             # https://www.tensorflow.org/lite/models/object_detection/overview
             # First label is '???', which has to be removed.
-            del(labels[0])
+    del(labels[0])
 
             # Load the Tensorflow Lite model.
             # If using Edge TPU, use special load_delegate argument
 
-            interpreter = Interpreter(model_path=PATH_TO_CKPT)
+    interpreter = Interpreter(model_path=PATH_TO_CKPT)
 
-            interpreter.allocate_tensors()
+    interpreter.allocate_tensors()
 
             # Get model details
-            input_details = interpreter.get_input_details()
-            output_details = interpreter.get_output_details()
-            height = input_details[0]['shape'][1]
-            width = input_details[0]['shape'][2]
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
 
-            floating_model = (input_details[0]['dtype'] == np.float32)
+    floating_model = (input_details[0]['dtype'] == np.float32)
 
-            input_mean = 127.5
-            input_std = 127.5
+    input_mean = 127.5
+    input_std = 127.5
 
 
 
@@ -71,12 +72,12 @@ def humancheck():
     image = cv2.imread(img)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     imH, imW, _ = image.shape
-    image_resized = cv2.resize(image_rgb, (width, height))
+    image_resized = cv2.resize(image_rgb, (height, width))
     input_data = np.expand_dims(image_resized, axis=0)
 
     # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
     if floating_model:
-    input_data = (np.float32(input_data) - input_mean) / input_std
+        input_data = (np.float32(input_data) - input_mean) / input_std
 
     # Perform the actual detection by running the model with the image as input
     interpreter.set_tensor(input_details[0]['index'],input_data)
@@ -94,6 +95,11 @@ def humancheck():
             object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
             if object_name == 'person': #if label is person stop looking and label it as a human
                 human = True
-                print('Human found!', scores[i]*100,"%")#Prints message and confidance percentage
-                shutil.move(img,img[:27] + "humans/"+ img[27:])
+
                 break
+    if human==True:
+        shutil.move(img,img[:27] + "humans/"+ img[27:])
+        print('Human found!',scores[i]*100,'%')
+
+    else:
+        print('No humans found')
